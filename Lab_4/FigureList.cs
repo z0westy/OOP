be1a198bc84e647cf43560846113bd1bf5656990
Prometey <zirko4@yandex.ru>
2026-05-04 05:16:32 +0300

    Lab_4

---
+++ Lab_4/FigureList.cs
@@ -0,0 +1,41 @@
+﻿// Class for figure list
+[Serializable]
+public class FigureList
+{
+    public int Count = 0;
+    public List<Figure> figures = new List<Figure>();
+
+    public IEnumerator<Figure> GetEnumerator()
+    {
+        for (int i = 0; i < figures.Count; i++)
+        {
+            yield return figures[i];
+        }
+    }
+
+    public Figure this[int i]
+    {
+        get => figures[i];
+    }
+
+    // Function for adding figure to list
+    public void addFigure(Figure f) 
+    { 
+        figures.Add(f);
+        Count++;
+    }
+
+    // Function for removing figure from list by value
+    public void removeFigure(Figure f) 
+    { 
+        figures.Remove(f);
+        Count--;
+    }
+
+    // Function for removing figure from list by index
+    public void removeFigureAt(int index)
+    {
+        figures.RemoveAt(index);
+        Count--;
+    }
+}
---
+++ Lab_4/fCreateFigure.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Lab_2
+{
+    partial class fCreateFigure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCreateFigure = new Button();
+            SuspendLayout();
+            // 
+            // btnCreateFigure
+            // 
+            btnCreateFigure.Location = new Point(12, 61);
+            btnCreateFigure.Name = "btnCreateFigure";
+            btnCreateFigure.Size = new Size(156, 37);
+            btnCreateFigure.TabIndex = 8;
+            btnCreateFigure.Text = "Создать";
+            btnCreateFigure.UseVisualStyleBackColor = true;
+            btnCreateFigure.Click += btnCreateFigure_Click;
+            // 
+            // fCreateFigure
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(259, 151);
+            Controls.Add(btnCreateFigure);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "fCreateFigure";
+            Text = "Создание фигуры";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnCreateFigure;
+    }
+}
